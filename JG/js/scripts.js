5f1610734cfd652cbcd1298258c78231503575d0
hth225 <hth225@naver.com>
2018-06-11 17:44:45 +0900

    .

--- JG/js/scripts.js
+++ JG/js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "17 july 2018 00:00:00", /** Enter new date here **/
+		date: "4 Sept 2018 00:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
